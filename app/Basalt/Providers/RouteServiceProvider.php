b42d69fa9449f648ef6bd6fc47cca7dc1b844005
Albert <w.albert221@gmail.com>
2014-12-05 22:48:33 +0100

    Moved routes loading to RouteServiceProvider.

--- app/Basalt/Providers/RouteServiceProvider.php
+++ app/Basalt/Providers/RouteServiceProvider.php
@@ -7,6 +7,8 @@ class RouteServiceProvider extends ServiceProvider
 {
     public function provide()
     {
-        //
+        $this->app->container->routes = function() {
+            return require dirname(dirname(dirname(__FILE__))).'/routes.php';
+        };
     }
 }
