a807a44d0daff898f0dd75349ab72e943bd47b1b
Albert221 <w.albert221@gmail.com>
2015-12-11 18:09:46 +0100

    Added PhpDocs for RoutesFacade and renamed a variable.

--- app/Basalt/Facades/RoutesFacade.php
+++ app/Basalt/Facades/RoutesFacade.php
@@ -8,13 +8,33 @@ use Symfony\Component\Routing\RouteCollection;
 
 class RoutesFacade
 {
+    /**
+     * @var RouteCollection Route collection.
+     */
     protected $routeCollection;
 
+    /**
+     * RoutesFacade constructor.
+     * @param RouteCollection $routeCollection
+     */
     public function __construct(RouteCollection $routeCollection)
     {
         $this->routeCollection = $routeCollection;
     }
 
+    /**
+     * Adds route to route collection.
+     *
+     * @param string $name Route name
+     * @param string $path Route path
+     * @param string $action Route controller action
+     * @param array $methods Route methods
+     * @param array $defaults Route defaults
+     * @param array $requirements Route requirements
+     * @param array $options Route options
+     * @param string $host Route host
+     * @param array $schemes Route schemes
+     */
     public function add($name, $path, $action, $methods = [], $defaults = [], $requirements = [], $options = [], $host = '', $schemes = [])
     {
         $defaults = array_merge($defaults, [
@@ -26,26 +46,79 @@ class RoutesFacade
         $this->routeCollection->add($name, $route);
     }
 
+    /**
+     * Adds GET route to route collection.
+     *
+     * @param string $name Route name
+     * @param string $path Route path
+     * @param string $action Route controller action
+     * @param array $defaults Route defaults
+     * @param array $requirements Route requirements
+     * @param array $options Route options
+     * @param string $host Route host
+     * @param array $schemes Route schemes
+     */
     public function addGet($name, $path, $action, $defaults = [], $requirements = [], $options = [], $host = '', $schemes = [])
     {
         $this->add($name, $path, $action, Request::METHOD_GET, $defaults, $requirements, $options, $host, $schemes);
     }
 
+    /**
+     * Adds POST route to route collection.
+     *
+     * @param string $name Route name
+     * @param string $path Route path
+     * @param string $action Route controller action
+     * @param array $defaults Route defaults
+     * @param array $requirements Route requirements
+     * @param array $options Route options
+     * @param string $host Route host
+     * @param array $schemes Route schemes
+     */
     public function addPost($name, $path, $action, $defaults = [], $requirements = [], $options = [], $host = '', $schemes = [])
     {
         $this->add($name, $path, $action, Request::METHOD_POST, $defaults, $requirements, $options, $host, $schemes);
     }
 
+    /**
+     * Adds PUT route to route collection.
+     *
+     * @param string $name Route name
+     * @param string $path Route path
+     * @param string $action Route controller action
+     * @param array $defaults Route defaults
+     * @param array $requirements Route requirements
+     * @param array $options Route options
+     * @param string $host Route host
+     * @param array $schemes Route schemes
+     */
     public function addPut($name, $path, $action, $defaults = [], $requirements = [], $options = [], $host = '', $schemes = [])
     {
         $this->add($name, $path, $action, Request::METHOD_PUT, $defaults, $requirements, $options, $host, $schemes);
     }
 
+    /**
+     * Adds DELETE route to route collection.
+     *
+     * @param string $name Route name
+     * @param string $path Route path
+     * @param string $action Route controller action
+     * @param array $defaults Route defaults
+     * @param array $requirements Route requirements
+     * @param array $options Route options
+     * @param string $host Route host
+     * @param array $schemes Route schemes
+     */
     public function addDelete($name, $path, $action, $defaults = [], $requirements = [], $options = [], $host = '', $schemes = [])
     {
         $this->add($name, $path, $action, Request::METHOD_DELETE, $defaults, $requirements, $options, $host, $schemes);
     }
 
+    /**
+     * Return route collection.
+     *
+     * @return RouteCollection Route collecton
+     */
     public function getRouteCollection()
     {
         return $this->routeCollection;
--- app/routes.php
+++ app/routes.php
@@ -3,27 +3,27 @@
 use Basalt\Facades\RoutesFacade;
 use Symfony\Component\Routing\RouteCollection;
 
-$facade = new RoutesFacade(new RouteCollection);
+$routes = new RoutesFacade(new RouteCollection);
 
-$facade->addGet('index', '/', 'Basalt\\Http\\Controllers\\PagesController@page');
+$routes->addGet('index', '/', 'Basalt\\Http\\Controllers\\PagesController@page');
 
-$facade->addGet('dashboard', '/admin', 'Basalt\\Http\\Controllers\\AdminPanelController@dashboard');
+$routes->addGet('dashboard', '/admin', 'Basalt\\Http\\Controllers\\AdminPanelController@dashboard');
 
-$facade->addGet('pages', '/admin/pages', 'Basalt\\Http\\Controllers\\PagesController@pages');
-$facade->addGet('newPage', '/admin/pages/new', 'Basalt\\Http\\Controllers\\PagesController@newPage');
-$facade->addPost('addPage', '/admin/pages', 'Basalt\\Http\\Controllers\\PagesController@add');
-$facade->addGet('editPage', '/admin/pages/{id}', 'Basalt\\Http\\Controllers\\PagesController@edit');
-$facade->addPut('updatePage', '/admin/pages/{id}', 'Basalt\\Http\\Controllers\\PagesController@update');
-$facade->addPost('changePagesOrder', '/admin/', 'Basalt\\Http\\Controllers\\PagesController@changeOrder');
-$facade->addDelete('deletePage', '/admin/{id}', 'Basalt\\Http\\Controllers\\PagesController@delete');
+$routes->addGet('pages', '/admin/pages', 'Basalt\\Http\\Controllers\\PagesController@pages');
+$routes->addGet('newPage', '/admin/pages/new', 'Basalt\\Http\\Controllers\\PagesController@newPage');
+$routes->addPost('addPage', '/admin/pages', 'Basalt\\Http\\Controllers\\PagesController@add');
+$routes->addGet('editPage', '/admin/pages/{id}', 'Basalt\\Http\\Controllers\\PagesController@edit');
+$routes->addPut('updatePage', '/admin/pages/{id}', 'Basalt\\Http\\Controllers\\PagesController@update');
+$routes->addPost('changePagesOrder', '/admin/', 'Basalt\\Http\\Controllers\\PagesController@changeOrder');
+$routes->addDelete('deletePage', '/admin/{id}', 'Basalt\\Http\\Controllers\\PagesController@delete');
 
-$facade->addGet('plugins', '/admin/plugins', 'Basalt\\Http\\Controllers\\PluginsController@plugins');
+$routes->addGet('plugins', '/admin/plugins', 'Basalt\\Http\\Controllers\\PluginsController@plugins');
 
-$facade->addGet('settings', '/admin/settings', 'Basalt\\Http\\Controllers\\SettingsController@settings');
-$facade->addPost('updateSettings', '/admin/settings', 'Basalt\\Http\\Controllers\\SettingsController@update');
+$routes->addGet('settings', '/admin/settings', 'Basalt\\Http\\Controllers\\SettingsController@settings');
+$routes->addPost('updateSettings', '/admin/settings', 'Basalt\\Http\\Controllers\\SettingsController@update');
 
-$facade->addGet('updates', '/admin/updates', 'Basalt\\Http\\Controllers\\UpdatesController@updates');
+$routes->addGet('updates', '/admin/updates', 'Basalt\\Http\\Controllers\\UpdatesController@updates');
 
-$facade->addGet('page', '/{slug}', 'Basalt\\Http\\Controllers\\PagesController@page');
+$routes->addGet('page', '/{slug}', 'Basalt\\Http\\Controllers\\PagesController@page');
 
-return $facade->getRouteCollection();
+return $routes->getRouteCollection();
